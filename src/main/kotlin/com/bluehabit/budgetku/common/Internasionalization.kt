d9234ce403cdc3e264b3ac0c9c509e670b34e36f
Trian Damai <triannurizkillah@gmail.com>
2023-02-26 11:08:29 +0700

    add copyright

--- src/main/kotlin/com/bluehabit/budgetku/common/Internasionalization.kt
+++ src/main/kotlin/com/bluehabit/budgetku/common/Internasionalization.kt
@@ -1,8 +1,14 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 package com.bluehabit.budgetku.common
 
 import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.context.support.ResourceBundleMessageSource
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType.Object
 
 fun ResourceBundleMessageSource.translate(key:String):String{
     return getMessage(key,null,LocaleContextHolder.getLocale())
