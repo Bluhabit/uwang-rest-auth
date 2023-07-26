7e5f627dbec782e65284a8a30123643c0d6248ad
trian damai <triannurizkillah@gmail.com>
2023-07-26 14:54:08 +0700

    feat: update

---
+++ src/main/java/com/bluehabit/eureka/component/UserRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.component;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends PagingAndSortingRepository<User, String>, CrudRepository<User, String> {
+    Optional<User> findByUserEmail(String userEmail);
+}
