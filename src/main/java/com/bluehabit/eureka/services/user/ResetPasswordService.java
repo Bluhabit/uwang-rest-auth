bad309aa98b77b906699679899f95c3df559f10e
saddam-satria <ardhisaddam@gmail.com>
2023-08-11 18:35:20 +0700

    [FIX][GAWE-53] fix several suggetions

--- src/main/java/com/bluehabit/eureka/services/user/ResetPasswordService.java
+++ src/main/java/com/bluehabit/eureka/services/user/ResetPasswordService.java
@@ -14,7 +14,7 @@ import com.bluehabit.eureka.component.user.UserCredential;
 import com.bluehabit.eureka.component.user.UserCredentialRepository;
 import com.bluehabit.eureka.component.user.UserVerification;
 import com.bluehabit.eureka.component.user.UserVerificationRepository;
-import com.bluehabit.eureka.exception.UnAuthorizedException;
+import com.bluehabit.eureka.exception.GeneralErrorException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,21 +31,16 @@ public class ResetPasswordService extends AbstractBaseService {
 
     public ResponseEntity<BaseResponse<Object>> reset(String token, ResetPasswordRequest request) {
         final Optional<UserVerification> userVerification = userVerificationRepository.findByToken(token);
-        try {
 
-            if (userVerification.isEmpty()) {
-                throw new UnAuthorizedException(HttpStatus.NOT_FOUND.value(), translate("auth.token.invalid"));
-            }
-            final UserVerification userVerified = userVerification.get();
-            final UserCredential userCredential = userVerified.getUser();
-            userCredential.setUserPassword(request.newPassword());
-            userCredentialRepository.save(userCredential);
-
-            return BaseResponse.success(translate("auth.success"), new HashMap<>());
-
-        } catch (UnAuthorizedException exception) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(BaseResponse.error(exception.getStatusCode(), exception.getMessage()));
+        if (userVerification.isEmpty()) {
+            throw new GeneralErrorException(HttpStatus.NOT_FOUND.value(), translate("auth.token.invalid"));
         }
+        final UserVerification userVerified = userVerification.get();
+        final UserCredential userCredential = userVerified.getUser();
+        userCredential.setUserPassword(request.newPassword());
+        userCredentialRepository.save(userCredential);
+
+        return BaseResponse.success(translate("auth.success"), new HashMap<>());
 
     }
 }
