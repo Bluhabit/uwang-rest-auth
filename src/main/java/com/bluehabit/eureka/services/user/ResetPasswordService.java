373aafa8859d416f703de7a2b2fdec4577e09347
saddam-satria <ardhisaddam@gmail.com>
2023-08-11 19:59:37 +0700

    [FIX][GAWE-53] merge main to sync auth controller and fix some reviewed

--- src/main/java/com/bluehabit/eureka/services/user/ResetPasswordService.java
+++ src/main/java/com/bluehabit/eureka/services/user/ResetPasswordService.java
@@ -9,6 +9,7 @@ package com.bluehabit.eureka.services.user;
 
 import com.bluehabit.eureka.common.AbstractBaseService;
 import com.bluehabit.eureka.common.BaseResponse;
+import com.bluehabit.eureka.component.user.UserCredential;
 import com.bluehabit.eureka.component.user.model.ResetPasswordRequest;
 import com.bluehabit.eureka.component.user.UserCredentialRepository;
 import com.bluehabit.eureka.component.user.UserVerification;
@@ -35,7 +36,9 @@ public class ResetPasswordService extends AbstractBaseService {
             throw new GeneralErrorException(HttpStatus.NOT_FOUND.value(), translate("auth.token.invalid"));
         }
         final UserVerification userVerified = userVerification.get();
-
+        final UserCredential userCredential = userVerified.getUser();
+        userCredential.setPassword(request.newPassword());
+        userCredentialRepository.save(userCredential);
         return BaseResponse.success(translate("auth.success"), new HashMap<>());
 
     }
