71ff234753bd70039a04634e998f1d89a27034cb
trian damai <triannurizkillah@gmail.com>
2023-08-18 00:24:37 +0700

    fix: make sure otp length less than 4 character

--- src/main/java/com/bluehabit/eureka/common/OtpGenerator.java
+++ src/main/java/com/bluehabit/eureka/common/OtpGenerator.java
@@ -16,12 +16,15 @@ public class OtpGenerator {
     private static final int MIN_OTP_VALUE = 1000;
     private static final int MAX_OTP_VALUE = 9999;
 
+    private static final int MAX_LENGTH_OTP = 4;
+    private static final int MAX_INDEX_OTP = 3;
+
     public static String generateOtp() {
         final Random random = new Random();
         final int otp = MIN_OTP_VALUE + random.nextInt(MAX_OTP_VALUE);
         final String resultOtp = String.valueOf(otp);
-        if (resultOtp.length() > 4) {
-            return resultOtp.substring(3, resultOtp.length() - 1);
+        if (resultOtp.length() > MAX_LENGTH_OTP) {
+            return resultOtp.substring(MAX_INDEX_OTP, resultOtp.length() - 1);
         }
         return resultOtp;
     }
