c6ddb4ac8e676880680967bf59e11bd4467730dc
trian damai <triannurizkillah@gmail.com>
2023-08-22 00:29:44 +0700

    patch: update some broken entity

---
+++ src/main/java/com/bluehabit/eureka/component/data/UserCredential.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.component.data;
+
+import com.bluehabit.eureka.component.AuthProvider;
+import com.bluehabit.eureka.component.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user_credential")
+@SQLDelete(
+    sql = "UPDATE tb_user_credential SET deleted=true WHERE id=?"
+)
+@Where(
+    clause = "deleted = false"
+)
+public class UserCredential {
+    @Id
+    @GenericGenerator(
+        name = "UUID",
+        type = org.hibernate.id.uuid.UuidGenerator.class
+    )
+    private String id;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(columnDefinition = "int2")
+    private AuthProvider authProvider;
+    @Enumerated(EnumType.ORDINAL)
+    @Column
+    private UserStatus status;
+    @OneToMany
+    private List<UserProfile> userInfo;
+    @Column
+    private OffsetDateTime createdAt;
+    @Column
+    private OffsetDateTime updatedAt;
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted;
+
+    public Map<String, Object> toResponse() {
+        final Map<String, Object> data = new HashMap<>();
+        data.put("id", getId());
+        data.put("email", getEmail());
+        data.put("status", getStatus());
+        data.put("userInfo", getUserInfo().stream().map(info -> Map.of(info.getKey(), info.getValue())));
+        data.put("createdAt", getCreatedAt());
+        data.put("updatedAt", getUpdatedAt());
+
+        return data;
+    }
+}
--- src/main/java/com/bluehabit/eureka/services/SignInService.java
+++ src/main/java/com/bluehabit/eureka/services/SignInService.java
@@ -9,14 +9,15 @@ package com.bluehabit.eureka.services;
 
 import com.bluehabit.eureka.common.AbstractBaseService;
 import com.bluehabit.eureka.common.BaseResponse;
-import com.bluehabit.eureka.common.Constant;
 import com.bluehabit.eureka.common.GoogleAuthUtil;
 import com.bluehabit.eureka.common.JwtUtil;
-import com.bluehabit.eureka.component.user.UserCredential;
-import com.bluehabit.eureka.component.user.UserCredentialRepository;
-import com.bluehabit.eureka.component.user.model.SignInResponse;
-import com.bluehabit.eureka.component.user.model.SignInWithEmailRequest;
-import com.bluehabit.eureka.component.user.model.SignInWithGoogleRequest;
+import com.bluehabit.eureka.component.AuthProvider;
+import com.bluehabit.eureka.component.UserStatus;
+import com.bluehabit.eureka.component.data.UserCredential;
+import com.bluehabit.eureka.component.data.UserCredentialRepository;
+import com.bluehabit.eureka.component.model.SignInResponse;
+import com.bluehabit.eureka.component.model.SignInWithEmailRequest;
+import com.bluehabit.eureka.component.model.SignInWithGoogleRequest;
 import com.bluehabit.eureka.exception.GeneralErrorException;
 import com.bluehabit.eureka.exception.UnAuthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,38 +44,40 @@ public class SignInService extends AbstractBaseService {
 
     public ResponseEntity<BaseResponse<SignInResponse>> signInWithGoogle(SignInWithGoogleRequest request) {
         validate(request);
-        return GoogleAuthUtil.getGoogleClaim(request.token()).map(googleClaim -> {
-                final String jwtToken = jwtUtil.generateToken(googleClaim.email());
-                return userCredentialRepository
-                    .findByEmail(googleClaim.email()).map(user -> {
-                        if (!user.getAuthProvider().equals(Constant.AUTH_GOOGLE)) {
-                            throw new UnAuthorizedException(translate("auth.method.provider.not.match"));
-                        }
+        return GoogleAuthUtil.getGoogleClaim(request.token())
+            .map(googleClaim -> {
+                    final String jwtToken = jwtUtil.generateToken(googleClaim.email());
+                    return userCredentialRepository
+                        .findByEmail(googleClaim.email())
+                        .map(user -> {
+                            if (!user.getAuthProvider().equals(AuthProvider.GOOGLE)) {
+                                throw new UnAuthorizedException(translate("auth.method.provider.not.match"));
+                            }
 
-                        return BaseResponse.success(
-                            translate("auth.success"),
-                            new SignInResponse(jwtToken, user)
-                        );
-                    }).orElseGet(() -> {
-                        final String uuid = UUID.randomUUID().toString();
-                        final OffsetDateTime currentDate = OffsetDateTime.now();
+                            return BaseResponse.success(
+                                translate("auth.success"),
+                                new SignInResponse(jwtToken, user.toResponse())
+                            );
+                        }).orElseGet(() -> {
+                            final String uuid = UUID.randomUUID().toString();
+                            final OffsetDateTime currentDate = OffsetDateTime.now();
 
-                        final UserCredential userCredential = new UserCredential();
-                        userCredential.setId(uuid);
-                        userCredential.setEmail(googleClaim.email());
-                        userCredential.setAuthProvider(Constant.AUTH_GOOGLE);
-                        userCredential.setActive(Constant.USER_ACTIVE);
-                        userCredential.setCreatedAt(currentDate);
-                        userCredential.setUpdatedAt(currentDate);
-                        final UserCredential saved = userCredentialRepository.save(userCredential);
+                            final UserCredential userCredential = new UserCredential();
+                            userCredential.setId(uuid);
+                            userCredential.setEmail(googleClaim.email());
+                            userCredential.setAuthProvider(AuthProvider.GOOGLE);
+                            userCredential.setStatus(UserStatus.ACTIVE);
+                            userCredential.setCreatedAt(currentDate);
+                            userCredential.setUpdatedAt(currentDate);
+                            final UserCredential saved = userCredentialRepository.save(userCredential);
 
-                        return BaseResponse.success(
-                            translate("auth.success"),
-                            new SignInResponse(jwtToken, saved)
-                        );
-                    });
-            }
-        ).orElseThrow(() -> new UnAuthorizedException(translate("auth.token.invalid")));
+                            return BaseResponse.success(
+                                translate("auth.success"),
+                                new SignInResponse(jwtToken, saved.toResponse())
+                            );
+                        });
+                }
+            ).orElseThrow(() -> new UnAuthorizedException(translate("auth.token.invalid")));
     }
 
     public ResponseEntity<BaseResponse<SignInResponse>> signIn(@RequestBody SignInWithEmailRequest request) {
@@ -87,14 +90,14 @@ public class SignInService extends AbstractBaseService {
                     throw new GeneralErrorException(HttpStatus.BAD_REQUEST.value(), translate("auth.invalid"));
                 }
 
-                if (!user.getAuthProvider().equals(Constant.AUTH_BASIC)) {
+                if (!user.getAuthProvider().equals(AuthProvider.BASIC)) {
                     throw new GeneralErrorException(HttpStatus.BAD_REQUEST.value(), translate("auth.invalid"));
                 }
 
                 final String token = jwtUtil.generateToken(request.email());
                 return BaseResponse.success(
                     translate("auth.success"),
-                    new SignInResponse(token, user)
+                    new SignInResponse(token, user.toResponse())
                 );
             }
         ).orElseThrow(() -> new GeneralErrorException(HttpStatus.NOT_FOUND.value(), translate("auth.invalid")));
