24c42eea2c6541417efdac770c0c051be494d9ef
trian damai <triannurizkillah@gmail.com>
2023-08-22 18:58:53 +0700

    patch: update some broken entity

--- src/main/java/com/bluehabit/eureka/config/ApplicationConfig.java
+++ src/main/java/com/bluehabit/eureka/config/ApplicationConfig.java
@@ -11,9 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class ApplicationConfig {
@@ -28,14 +25,7 @@ public class ApplicationConfig {
         loggingFilter.setMaxPayloadLength(maxPayload);
         return loggingFilter;
     }
-
-    @Bean
-    public SessionLocaleResolver localResolver() {
-        final SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(Locale.US);
-        return resolver;
-    }
-
+    
     @Bean
     public ResourceBundleMessageSource bundleMessageSource() {
         final ResourceBundleMessageSource message = new ResourceBundleMessageSource();
