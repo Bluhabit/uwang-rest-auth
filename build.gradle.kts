6c1f68eb16b7c8c7bef81a1964a46502cad38082
trian damai <triannurizkillah@gmail.com>
2023-08-24 18:39:38 +0700

    patch: update build artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ repositories {
 
 dependencies {
     implementation("org.projectlombok:lombok:1.18.28")
+    testImplementation("junit:junit:4.13.1")
     annotationProcessor("org.projectlombok:lombok:1.18.28")
 
     testImplementation("org.projectlombok:lombok:1.18.28")
@@ -39,6 +40,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
