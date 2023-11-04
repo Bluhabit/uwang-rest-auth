84369a7a020c35fec3fc48f0cdb7df703fcbfcf4
trian damai <triannurizkillah@gmail.com>
2023-11-04 10:09:23 +0700

    feat: initial commit uwang(dev)

--- src/main/java/com/bluehabit/blu/EurekaApplication.java
+++ src/main/java/com/bluehabit/blu/EurekaApplication.java
@@ -8,12 +8,9 @@
 package com.bluehabit.blu;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @PropertySource("classpath:build.properties")
