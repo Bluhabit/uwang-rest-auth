299375d4fa2fd1c59bae43a41c447e430ee9b8de
trian damai <triannurizkillah@gmail.com>
2023-11-19 18:25:48 +0700

    feat: initial commit dev

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ async fn main() -> std::io::Result<()> {
             }))
             .configure(init)
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 4000))?
         .run()
         .await
 }
